8c4e5c61b3fa9529eb69c9d616f85de94e1cdc4f
Kamran Elchuzade <elchuzade@gmail.com>
2020-05-15 20:42:17 +0300

    added drawing functions

--- core.py
+++ core.py
@@ -65,15 +65,22 @@ class Game:
         self.map = helpers.initialize_map_and_gold(
             self.gnome, gold_amount, constants.CELLS, constants.EXIT, [constants.WALL_1, constants.WALL_2])
 
+        self.exit = constants.EXIT
         self.step_counter = 0
         self.collected_gold = 0
         # state_size is input of training model
         # 4 stands for gnome_x, gnome_y, distance_to_exit, collected_gold
-        self.state_size = helpers.get_state_size(constants.GNOME_VISION_SIZE) + 4
+        self.state_size = helpers.get_state_size(constants.GNOME["vision_size"]) + 4
         # action_size is output of training model
         self.action_size = len(constants.POSSIBLE_ACTIONS["gnome"])
         self.model = Model()
 
+    def get_gold(self):
+        return self.collected_gold
+
+    def get_exit(self):
+        return helpers.find_exit_distance(self.exit, self.gnome)
+    
     def get_gnome_vision(self):
         return helpers.get_gnome_vision(self.map, self.gnome)
 
@@ -90,38 +97,105 @@ class Game:
             # Move gnome in the given direction if not next ot the wall
             if direction == 0:
                 # Check if there is a wall or boundary on the left by finding the center of the gnome's vision
-                if self.get_gnome_vision()[self.gnome.vision_size][self.gnome.vision_size - 1] != 0 or \
+                if self.get_gnome_vision()[self.gnome.vision_size][self.gnome.vision_size - 1] != 0 and \
                         self.get_gnome_vision()[self.gnome.vision_size][self.gnome.vision_size - 1] != 4:
                     make_step(0)
 
             elif direction == 1:
                 # Check if there is a wall or boundary on the top by finding the center of the gnome's vision
-                if self.get_gnome_vision()[self.gnome.vision_size - 1][self.gnome.vision_size] != 0 or \
+                if self.get_gnome_vision()[self.gnome.vision_size - 1][self.gnome.vision_size] != 0 and \
                         self.get_gnome_vision()[self.gnome.vision_size - 1][self.gnome.vision_size] != 4:
                     make_step(1)
 
             elif direction == 2:
                 # Check if there is a wall or boundary on the right by finding the center of the gnome's vision
-                if self.get_gnome_vision()[self.gnome.vision_size][self.gnome.vision_size + 1] != 0 or \
+                if self.get_gnome_vision()[self.gnome.vision_size][self.gnome.vision_size + 1] != 0 and \
                         self.get_gnome_vision()[self.gnome.vision_size][self.gnome.vision_size + 1] != 4:
                     make_step(2)
 
             elif direction == 3:
                 # Check if there is a wall or boundary on the bottom by finding the center of the gnome's vision
-                if self.get_gnome_vision()[self.gnome.vision_size + 1][self.gnome.vision_size] != 0 or \
+                if self.get_gnome_vision()[self.gnome.vision_size + 1][self.gnome.vision_size] != 0 and \
                         self.get_gnome_vision()[self.gnome.vision_size + 1][self.gnome.vision_size] != 4:
                     make_step(3)
 
             # Check if gnome has stepped on a gold
-            if helpers.check_new_cell(self.map, self.gnome, Map.GOLD):
+            if helpers.check_new_cell(self.map, self.gnome, Map.GOLD.value):
                 self.collected_gold += 1
                 print('collected gold - ', self.collected_gold)
 
             # Check if gnome has stepped on the exit
-            if helpers.check_new_cell(self.map, self.gnome, Map.EXIT):
+            if helpers.check_new_cell(self.map, self.gnome, Map.EXIT.value):
                 print('Congratulations!\nExit Found!')
-
-            print('step counter -', self.step_counter)
 
             self.map = helpers.add_new_gnome(self.map, self.gnome)
 
+    def __initialize_game(self):
+        pygame.init()
+        pygame.display.set_caption("Gnome game by {}".format(self.__mode))
+        size = constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT
+        screen = pygame.display.set_mode(size)
+        font = pygame.font.Font(constants.FONT_NAME, constants.FONT_SIZE)
+
+        # Clock is set to keep track of frames
+        clock = pygame.time.Clock()
+        pygame.display.flip()
+        frame = 1
+        action_taken = False
+        while True:
+            clock.tick(constants.FPS)
+            pygame.event.pump()
+            for event in pygame.event.get():
+                if self.__mode == "player" and not action_taken:
+                    # Look for any button press action
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_LEFT:
+                            action_taken = True
+                            self.step(0)
+
+                        elif event.key == pygame.K_UP:
+                            action_taken = True
+                            self.step(1)
+
+                        elif event.key == pygame.K_RIGHT:
+                            action_taken = True
+                            self.step(2)
+
+                        elif event.key == pygame.K_DOWN:
+                            action_taken = True
+                            self.step(3)
+                        else:
+                            print(self.get_gnome_vision())
+
+                # Quit the game if the X symbol is clicked
+                if event.type == pygame.QUIT:
+                    print("pressing escape")
+                    pygame.quit()
+                    raise SystemExit
+                
+            action_taken = False
+            # Build up a black screen as a game background
+            screen.fill(constants.GAME_BACKGROUND)
+
+            helpers.draw_game(screen, self.gnome, self.get_gnome_vision())
+
+            # gold_text_placeholder, gold_rect_text_placeholder = helpers.update_gold_text_placeholder(font)
+            # screen.blit(gold_text_placeholder, gold_rect_text_placeholder)
+            #
+            # exit_text_placeholder, exit_rect_text_placeholder = helpers.update_exit_text_placeholder(font)
+            # screen.blit(exit_text_placeholder, exit_rect_text_placeholder)
+            #
+            # gold_text, gold_text_rect = helpers.update_gold_text(font, self.get_gold())
+            # screen.blit(gold_text, gold_text_rect)
+            #
+            # exit_text, exit_text_rect = helpers.update_exit_text(font, self.get_exit())
+            # screen.blit(exit_text, exit_text_rect)
+
+            # update display
+            pygame.display.flip()
+            frame += 1
+
+    def play(self):
+        if self.__mode == "ai":
+            print("ai playing")
+        self.__initialize_game()
--- helpers.py
+++ helpers.py
@@ -1,5 +1,8 @@
 import random
+import constants
 import pygame
+import core
+from enum import Enum
 
 """
 MAP has a map of values
@@ -10,6 +13,15 @@ MAP has a map of values
     4 - wall
     5 - exit
 """
+
+
+class Map(Enum):
+    BOUNDARY = 0
+    EMPTY = 1
+    GNOME = 2
+    GOLD = 3
+    WALL = 4
+    EXIT = 5
 
 
 def print_map(map):
@@ -40,7 +52,7 @@ def get_gnome_vision_flat(map, gnome):
 
 
 def remove_old_gnome(map, gnome):
-    map[gnome.y + gnome.vision_size][gnome.x + gnome.vision_size] = 1
+    map[gnome.y + gnome.vision_size][gnome.x + gnome.vision_size] = Map.EMPTY.value
     return map
 
 
@@ -51,12 +63,15 @@ def check_new_cell(map, gnome, option):
 
 
 def add_new_gnome(map, gnome):
-    map[gnome.y + gnome.vision_size][gnome.x + gnome.vision_size] = 2
+    map[gnome.y + gnome.vision_size][gnome.x + gnome.vision_size] = Map.GNOME.value
     return map
 
 
+def find_exit_distance(exit, gnome):
+    return abs(exit["y"] - gnome.y) + abs(exit["x"] - gnome.x)
+
+
 def initialize_map_and_gold(gnome, gold_amount, cells, exit, walls):
-    gold = []
     map = initialize_map(cells, gnome.vision_size)
     map = add_exit(map, exit, gnome.vision_size)
     map = add_gnome(map, gnome)
@@ -75,27 +90,27 @@ def initialize_map(cells, vision_size):
         for i in range(cells["x"] + 2 * vision_size):
             # Set for outside boundaries
             if i < vision_size or i >= vision_size + cells["x"] or j < vision_size or j >= vision_size + cells["y"]:
-                map_x_line.append(0)
+                map_x_line.append(Map.BOUNDARY.value)
             else:
-                map_x_line.append(1)
+                map_x_line.append(Map.EMPTY.value)
         map.append(map_x_line)
     return map
 
 
 def add_exit(map, exit, vision_size):
-    map[vision_size + exit["y"]][vision_size + exit["x"]] = 5
+    map[vision_size + exit["y"]][vision_size + exit["x"]] = Map.EXIT.value
     return map
 
 
 def add_gnome(map, gnome):
-    map[gnome.vision_size + gnome.y][gnome.vision_size + gnome.x] = 2
+    map[gnome.vision_size + gnome.y][gnome.vision_size + gnome.x] = Map.GNOME.value
     return map
 
 
 def add_walls(map, walls, vision_size):
     for wall in walls:
         for cell in wall:
-            map[vision_size + cell["y"]][vision_size + cell["x"]] = 4
+            map[vision_size + cell["y"]][vision_size + cell["x"]] = Map.WALL.value
     return map
 
 
@@ -104,7 +119,7 @@ def add_gold(map, gold_amount):
         _empty_cells = []
         for _i in range(len(map)):
             for _j in range(len(map[_i])):
-                if map[_i][_j] == 1:
+                if map[_i][_j] == Map.EMPTY.value:
                     _empty_cells.append({"x": _j, "y": _i})
         return _empty_cells
 
@@ -113,6 +128,106 @@ def add_gold(map, gold_amount):
     for i in range(gold_amount):
         index = random.randrange(len(empty_cells))
         value = empty_cells.pop(index)
-        map[value["y"]][value["x"]] = 3
+        map[value["y"]][value["x"]] = Map.GOLD.value
     return map
+
+
+""" DRAWING """
+
+
+def draw_game(screen, gnome, vision):
+    draw_vision(screen, gnome, vision)
+    draw_margins(screen)
+    draw_scoreboard(screen)
+    draw_grid(screen)
+    draw_gnome(screen, gnome)
+    
+
+def draw_vision(screen, gnome, vision):
+    def draw_vision_cell(vision_size):
+        pygame.draw.rect(screen, constants.GNOME_VISION_COLOR,
+                         ((col + gnome.x - vision_size) * constants.CELL_SIZE + constants.MARGIN,
+                          (row + gnome.y - vision_size) * constants.CELL_SIZE + constants.MARGIN,
+                          constants.CELL_SIZE, constants.CELL_SIZE))
+
+    def draw_gold(vision_size):
+        pygame.draw.circle(screen, constants.GOLD_COLOR,
+                           [int((gnome.x + col - vision_size) * constants.CELL_SIZE + constants.CELL_SIZE / 2) +
+                            constants.MARGIN,
+                            int((gnome.y + row - vision_size) * constants.CELL_SIZE + constants.CELL_SIZE / 2) +
+                            constants.MARGIN],
+                           int(constants.GOLD_RADIUS))
+
+    def draw_exit():
+        pygame.draw.rect(screen, constants.EXIT_COLOR, ((col + gnome.x - gnome.vision_size) * constants.CELL_SIZE
+                                                        + constants.MARGIN,
+                                                        (row + gnome.y - gnome.vision_size) * constants.CELL_SIZE
+                                                        + constants.MARGIN,
+                                                        constants.CELL_SIZE, constants.CELL_SIZE))
+
+    def draw_wall():
+        print(col, row)
+        pygame.draw.rect(screen, constants.WALL_COLOR, ((col + gnome.x - gnome.vision_size) * constants.CELL_SIZE
+                                                        + constants.MARGIN,
+                                                        (row + gnome.y - gnome.vision_size) * constants.CELL_SIZE
+                                                        + constants.MARGIN,
+                                                        constants.CELL_SIZE, constants.CELL_SIZE))
+    
+    for row in range(len(vision)):
+        for col in range(len(vision[row])):
+            draw_vision_cell(len(vision[0]) // 2)
+            if vision[row][col] == Map.GOLD.value:
+                draw_gold(len(vision[0]) // 2)
+            elif vision[row][col] == Map.EXIT.value:
+                draw_exit()
+            elif vision[row][col] == Map.WALL.value:
+                draw_wall()
+
+
+def draw_margins(screen):
+    # Left line margin
+    pygame.draw.rect(screen, constants.MARGIN_BACKGROUND, (0, constants.MARGIN,
+                                                           constants.MARGIN, constants.GAME_PLAY_HEIGHT))
+    # Right line margin
+    pygame.draw.rect(screen, constants.MARGIN_BACKGROUND,
+                     (constants.MARGIN + constants.GAME_PLAY_WIDTH, constants.MARGIN,
+                      constants.MARGIN, constants.GAME_PLAY_HEIGHT))
+    # Top line margin
+    pygame.draw.rect(screen, constants.MARGIN_BACKGROUND, (0, 0,
+                                                           constants.MARGIN * 2 + constants.GAME_PLAY_WIDTH,
+                                                           constants.MARGIN))
+    # Bottom line margin
+    pygame.draw.rect(screen, constants.MARGIN_BACKGROUND, (0, constants.MARGIN + constants.GAME_PLAY_HEIGHT,
+                                                           constants.MARGIN * 2 + constants.GAME_PLAY_WIDTH,
+                                                           constants.MARGIN))
+    
+
+def draw_scoreboard(screen):
+    pygame.draw.rect(screen, constants.SCOREBOARD_BACKGROUND, (0, constants.GAME_PLAY_HEIGHT + constants.MARGIN * 2,
+                                                               constants.GAME_PLAY_WIDTH + constants.MARGIN * 2,
+                                                               constants.SCOREBOARD_HEIGHT))
+    
+
+def draw_grid(screen):
+    # Draws a grid to separate each game cell
+    for i in range(constants.CELLS["x"] - 1):
+        pygame.draw.rect(screen, constants.GRID_LINE_COLOR, (constants.MARGIN + i * constants.CELL_SIZE +
+                                                             constants.CELL_SIZE - constants.GRID_LINE_WIDTH / 2,
+                                                             constants.MARGIN,
+                                                             constants.GRID_LINE_WIDTH,
+                                                             constants.CELL_SIZE * constants.CELLS["y"]))
+
+    for i in range(constants.CELLS["y"] - 1):
+        pygame.draw.rect(screen, constants.GRID_LINE_COLOR, (constants.MARGIN,
+                                                             constants.MARGIN + i * constants.CELL_SIZE +
+                                                             constants.CELL_SIZE - constants.GRID_LINE_WIDTH / 2,
+                                                             constants.CELL_SIZE * constants.CELLS["x"],
+                                                             constants.GRID_LINE_WIDTH))
+
+
+def draw_gnome(screen, gnome):
+    pygame.draw.circle(screen, constants.GNOME_COLOR,
+                       [int(gnome.x * constants.CELL_SIZE + constants.CELL_SIZE / 2) + constants.MARGIN,
+                        int(gnome.y * constants.CELL_SIZE + constants.CELL_SIZE / 2) + constants.MARGIN],
+                       int(constants.GNOME_RADIUS))
 
